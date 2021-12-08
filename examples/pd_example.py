505462614c46f27131957e00f1f6953dffc76bb3
jm12138 <2286040843@qq.com>
2021-12-08 18:28:01 +0800

    update example

--- examples/pd_example.py
+++ examples/pd_example.py
@@ -1,15 +1,10 @@
-# Copyright (2017-2021)
-# The Wormnet project
-# Mathias Lechner (mlechner@ist.ac.at)
+import paddle
 import numpy as np
 import paddle.nn as nn
 import kerasncp as kncp
 from paddle.optimizer import Adam
 from kerasncp.paddle import LTCCell
-import paddle
 from paddle.io import DataLoader, TensorDataset
-
-# nn.Module that unfolds a RNN cell into a sequence
 
 
 class RNNSequence(nn.Layer):
@@ -47,7 +42,16 @@ class SequenceLearner(paddle.Model):
         return [loss.numpy()]
 
     def eval_batch(self, inputs, labels=None):
-        return self.train_batch(inputs, labels, update=False)
+        x, y = inputs[0], labels[0]
+        y_hat = self.network.forward(x)
+        y_hat = y_hat.reshape(y.shape)
+        loss = self._loss(y_hat, y)
+        return [loss.numpy()]
+
+    def predict_batch(self, inputs):
+        x = inputs[0]
+        y_hat = self.network.forward(x)
+        return [x.numpy(), y_hat.numpy()]
 
 
 if __name__ == '__main__':
@@ -61,7 +65,8 @@ if __name__ == '__main__':
     data_x = np.expand_dims(data_x, axis=0).astype(
         np.float32)  # Add batch dimension
     # Target output is a sine with double the frequency of the input signal
-    data_y = np.sin(np.linspace(0, 6 * np.pi, N)).reshape([1, N, 1]).astype(np.float32)
+    data_y = np.sin(np.linspace(0, 6 * np.pi, N)
+                    ).reshape([1, N, 1]).astype(np.float32)
     data_x = paddle.to_tensor(data_x)
     data_y = paddle.to_tensor(data_y)
     print("data_y.shape: ", str(data_y.shape))
@@ -69,9 +74,8 @@ if __name__ == '__main__':
     wiring = kncp.wirings.FullyConnected(
         8, out_features)  # 16 units, 8 motor neurons
     ltc_cell = LTCCell(wiring, in_features)
-    dataloader = DataLoader(
-        TensorDataset([data_x, data_y]), batch_size=1, shuffle=True, num_workers=4
-    )
+    dataloader = DataLoader(TensorDataset(
+        [data_x, data_y]), batch_size=1, shuffle=True, num_workers=4)
 
     ltc_sequence = RNNSequence(ltc_cell)
     learn = SequenceLearner(ltc_sequence)
