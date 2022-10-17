339288594c62994d3d608b254a4d04e4da17b66d
Mathias Lechner <mathias.lechner@hotmail.com>
2022-10-17 11:54:49 +0200

    CfC copyright statement

--- ncps/torch/cfc.py
+++ ncps/torch/cfc.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Mathias Lechner
+# Copyright 2022 Mathias Lechner and Ramin Hasani
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
